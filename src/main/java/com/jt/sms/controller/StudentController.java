2b50aaf0225384d11002d041166c4d428a7854d4
Rakesh_790 <brakeshroshan098@gmail.com>
2025-06-20 20:15:51 +0530

    Student Managemet System Backend is complete

---
+++ src/main/java/com/jt/sms/controller/StudentController.java
@@ -0,0 +1,64 @@
+package com.jt.sms.controller;
+import com.jt.sms.dto.StudentDTO;
+import com.jt.sms.entity.Student;
+import com.jt.sms.service.StudentService;
+
+import jakarta.validation.Valid;
+
+import java.util.List;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.var;
+
+
+
+@RestController
+@RequestMapping("/api/v1/students")
+@RequiredArgsConstructor
+public class StudentController {
+    private final StudentService studentService;
+    @GetMapping
+    public List<StudentDTO> getStudents() {
+        return studentService.getStudents();
+    }
+
+    @GetMapping("/{id}")
+    public StudentDTO getStudentById(@PathVariable String id) {
+        var studentDTO = studentService.getStudentsById(id);
+        Student student = new Student();
+        BeanUtils.copyProperties(studentDTO, student);
+        return studentService.getStudentsById(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public StudentDTO createStudent(@Valid @RequestBody StudentDTO studentDTO) {
+        Student newStudent = new Student();
+        BeanUtils.copyProperties(studentDTO, newStudent);
+        return studentService.saveStudent(newStudent);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteAllStudent(@PathVariable String id) {
+        studentService.deleteStudentById(id);
+    }
+
+    @PutMapping("/update/{id}")
+    public StudentDTO updateStudent(@PathVariable String id, @RequestBody StudentDTO studentDTO){
+        Student student = new Student();
+        BeanUtils.copyProperties(studentDTO, student);
+        return studentService.updateStudentById(id, student);
+    }
+}
---
+++ src/main/java/com/jt/sms/exception/handleException.java
@@ -0,0 +1,87 @@
+package com.jt.sms.exception;
+
+import java.time.LocalDateTime;
+// import java.util.HashMap;
+// import java.util.Map;
+// import java.util.NoSuchElementException;
+import java.util.StringJoiner;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.validation.FieldError;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+// import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class handleException {
+    // Traditionally Exception Handle.
+    // @ExceptionHandler(NoSuchElementException.class)
+    // @ResponseStatus(HttpStatus.BAD_REQUEST)
+    // public Map<String, String> handleNoSuchElementException(NoSuchElementException e) {
+    //     var map = new HashMap<String, String>();
+    //     map.put("error", "No such element found");
+    //     map.put("message", e.getMessage());
+    //     map.put("timestamp", LocalDateTime.now().toString());
+    //     return map;
+    // }
+    // @ExceptionHandler(StudentNotFoundException.class)
+    // @ResponseStatus(HttpStatus.BAD_REQUEST)
+    // public Map<String, String> handleNoSuchElementException(StudentNotFoundException e) {
+    //     var map = new HashMap<String, String>();
+    //     map.put("error", "No such element found");
+    //     map.put("message", e.getMessage());
+    //     map.put("timestamp", LocalDateTime.now().toString());
+    //     return map;
+    // }
+
+    // @ExceptionHandler(StudentNotFoundException.class)
+    // public ProblemDetail handleNoSuchElementException(StudentNotFoundException e) {
+    //     ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
+    //     problemDetail.setTitle("Student Not Found");
+    //     problemDetail.setDetail(e.getMessage());
+    //     problemDetail.setProperty("timestamp", LocalDateTime.now().toString());
+    //     return problemDetail;
+    // }
+
+    @ExceptionHandler(StudentNotFoundException.class)
+    public ProblemDetail handleNoSuchElementException(StudentNotFoundException e) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, e.getMessage());
+        problemDetail.setTitle("Student Not Found");
+        problemDetail.setProperty("timestamp", LocalDateTime.now().toString());
+        return problemDetail;
+    }
+
+    //for Post Method Not Supported Exception
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ProblemDetail handleRequestNotSupportedException(HttpRequestMethodNotSupportedException e) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.METHOD_NOT_ALLOWED, e.getMessage());
+        problemDetail.setTitle("Method Not Allowed");
+        problemDetail.setProperty("timestamp", LocalDateTime.now().toString());
+        return problemDetail;
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ProblemDetail handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        var details = new StringJoiner(", ");
+
+        e.getAllErrors().forEach(error -> {
+            var errorMessage = error.getDefaultMessage();
+            var fieldName = ((FieldError)error).getField();
+            details.add(fieldName + "-> " + errorMessage);
+        });     
+
+        var problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.UNPROCESSABLE_ENTITY, details.toString());
+        problemDetail.setTitle("Invalid data");
+        problemDetail.setProperty("timestamp", LocalDateTime.now().toString());
+        return problemDetail;
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ProblemDetail handleException(Exception e) {
+        return ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+    }
+}
+
