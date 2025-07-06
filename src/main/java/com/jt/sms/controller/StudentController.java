9c700d2d43cec74c0b0a489a3bdb5fb70165898f
Rakesh_790 <brakeshroshan098@gmail.com>
2025-07-06 10:31:12 +0530

    modified

--- src/main/java/com/jt/sms/controller/StudentController.java
+++ src/main/java/com/jt/sms/controller/StudentController.java
@@ -19,7 +19,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import lombok.RequiredArgsConstructor;
-import lombok.experimental.var;
 
 
 
